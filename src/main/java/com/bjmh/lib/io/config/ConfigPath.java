41962b5aa4640d84be34f6132cd95eabdc696221
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-03 09:11:28 +0100

    Added some javadoc comments

--- src/main/java/com/bjmh/lib/io/config/ConfigPath.java
+++ src/main/java/com/bjmh/lib/io/config/ConfigPath.java
@@ -1,7 +1,22 @@
 package com.bjmh.lib.io.config;
 
+/**
+ * The {@code ConfigPath} does not extend the {@code ConfigNode} interface
+ * and instead it's used to navigate the {@code Configuration} data structre.
+ * 
+ * @see ConfigNode
+ * @see Configuration
+ */
 public interface ConfigPath {
+  /**
+   * 
+   * @return the next part in the path
+   */
   public String next();
-  
+
+  /**
+   * 
+   * @return is there anymore parts to this path
+   */
   public boolean hasNext();
 }
