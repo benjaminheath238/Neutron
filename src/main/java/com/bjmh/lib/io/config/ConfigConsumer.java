18f181d4d4b8baa11cc216bb8f3d352f89a6c65a
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-03 23:08:50 +0100

    Made subclass of Consumer

--- src/main/java/com/bjmh/lib/io/config/ConfigConsumer.java
+++ src/main/java/com/bjmh/lib/io/config/ConfigConsumer.java
@@ -1,6 +1,8 @@
 package com.bjmh.lib.io.config;
 
+import java.util.function.Consumer;
+
 @FunctionalInterface
-public interface ConfigConsumer {
+public interface ConfigConsumer extends Consumer<ConfigNode> {
   public void accept(ConfigNode node);
 }
