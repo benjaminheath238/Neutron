74dba3c9678f4b5ef9c1408140b8cd989574be49
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-03 18:11:06 +0100

    Format fix

--- src/main/java/com/bjmh/lib/io/TextIO.java
+++ src/main/java/com/bjmh/lib/io/TextIO.java
@@ -7,28 +7,31 @@ public class TextIO {
     byte[] bytes = new byte[len];
     while (true) {
       try {
-        if (System.in.read(bytes) > 0) return new String(bytes).replace("\n", "").trim();
+        if (System.in.read(bytes) > 0)
+          return new String(bytes).replace("\n", "").trim();
       } catch (IOException e) {
         e.printStackTrace();
       }
     }
   }
-  
+
   public static String next(int len, String... of) {
     java.util.Arrays.sort(of);
     while (true) {
       String input = next(len);
-      if (java.util.Arrays.binarySearch(of, input) >= 0) return input;
+      if (java.util.Arrays.binarySearch(of, input) >= 0)
+        return input;
     }
   }
-  
+
   public static String next(int len, String regex) {
     while (true) {
       String input = next(len);
-      if (input.matches(regex)) return input;
+      if (input.matches(regex))
+        return input;
     }
   }
-  
+
   public static void print(String text) {
     System.out.print(text);
   }
@@ -38,7 +41,8 @@ public class TextIO {
     for (int i = 0; i < args.length; i++) {
       while (true) {
         int index = builder.indexOf("{" + i + "}");
-        if (index == -1) break;
+        if (index == -1)
+          break;
         builder.replace(index, index + 3, (args[i] == null) ? "null" : args[i].toString());
       }
     }
