95c4aeac36230991d404c4aa270bd6289870bebc
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-03 08:19:37 +0100

    Removed backup files

--- config/ConfigOption.java
+++
@@ -1,7 +0,0 @@
-package com.bjmh.lib.io.config;
-
-public interface ConfigOption extends ConfigNode {
-  public String getValue();
-  
-  public void setValue(String value);
-}
--- config/ConfigPath.java
+++
@@ -1,7 +0,0 @@
-package com.bjmh.lib.io.config;
-
-public interface ConfigPath {
-  public String next();
-  
-  public boolean hasNext();
-}
