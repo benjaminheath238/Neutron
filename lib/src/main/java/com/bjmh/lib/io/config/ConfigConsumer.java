e86cce94b9776db9a95e7f1df08912bf1267bcc0
Benjamin Heath <benjaminheath238@gmail.com>
2022-07-21 11:16:39 +0100

    Removed old stuff

--- lib/src/main/java/com/bjmh/lib/io/config/ConfigConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.bjmh.lib.io.config;
-
-import java.util.function.Consumer;
-
-@FunctionalInterface
-public interface ConfigConsumer extends Consumer<ConfigNode> {
-    @Override
-    public void accept(ConfigNode node);
-}
--- lib/src/main/java/com/bjmh/lib/io/config/ParserMethod.java
+++
@@ -1,5 +0,0 @@
-package com.bjmh.lib.io.config;
-
-public interface ParserMethod {
-    public void parse(String line, Configuration config);
-}
